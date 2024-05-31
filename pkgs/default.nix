e5b63fc78f3021ee102eaa58c7d503286f4b48c3
hugo-berendi <hugo.berendi@outlook.de>
2024-05-31 21:08:16 +0200

    .

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,12 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
-# You can build them using 'nix build .#example'
-pkgs: {
-  # example = pkgs.callPackage ./example { };
+# You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
+
+{ pkgs ? (import ../nixpkgs.nix) { } }:
+let plymouthThemes = pkgs.callPackage (import ./plymouth-themes.nix) { }; in
+{
+  # example = pkgs.callPackage (import ./example.nix) {};
+  # vimclip = pkgs.callPackage (import ./vimclip.nix) { };
+  # homer = pkgs.callPackage (import ./homer.nix) { };
+
+  plymouthThemeCutsAlt = plymouthThemes.cuts_alt;
 }
