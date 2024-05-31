9b1e484174e3a2764157d4e65c161027aef86830
hugo-berendi <hugo.berendi@outlook.de>
2024-06-01 01:03:46 +0200

    .

--- home-manager/features/wayland/waybar.nix
+++ home-manager/features/wayland/waybar.nix
@@ -2,8 +2,8 @@
   programs.waybar = {
     enable = true;
 
-    # systemd.enable = true;
-    # systemd.target = "hyprland-session.target";
+    systemd.enable = true;
+    systemd.target = "hyprland-session.target";
   };
 
   stylix.targets.waybar = { enable = false; };
