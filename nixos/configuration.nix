aa7dcbe383547c86fa3ef654179ff68071adb761
hugo-berendi <hugo.berendi@outlook.de>
2024-05-31 21:06:18 +0200

    .

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,6 +12,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
+    ./common/optional/quietboot.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
