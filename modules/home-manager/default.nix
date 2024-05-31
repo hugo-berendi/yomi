18d66518e3bf96a1f14740d7efc53cf0629b7426
hugo-berendi <hugo.berendi@outlook.de>
2024-05-31 19:52:26 +0200

    .

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -6,7 +6,7 @@
   # Modules not yet added to the stable branch
   bemenu = import ./compat/bemenu.nix;
   hyprpaper = import ./compat/hyprpaper.nix;
-  # k9s = import ./compat/k9s.nix;
+  k9s = import ./compat/k9s.nix;
 
   # Personal things
   # dev = import ./dev.nix;
