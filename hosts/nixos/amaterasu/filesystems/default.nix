4243895bb983f2c4317de1912100e0fdc6fa9d1c
Kamachi <68025706+hugo-berendi@users.noreply.github.com>
2024-06-05 22:08:48 +0200

    Update default.nix

--- hosts/nixos/amaterasu/filesystems/default.nix
+++ hosts/nixos/amaterasu/filesystems/default.nix
@@ -6,6 +6,6 @@
   # Mark a bunch of paths as needed for boot
   fileSystems =
     lib.attrsets.genAttrs
-    ["/" "/nix" "/persist/data" "/persist/state" "/persist/local/cache" "/boot"]
+    ["/" "/nix" "/persist" "/boot"]
     (p: {neededForBoot = true;});
 }
