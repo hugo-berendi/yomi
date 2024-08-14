c801842ca92f9093f55f84399fd330591e266865
Hugo Berendi <hugo.berendi@outlook.de>
2024-08-14 17:12:12 +0200

    147 current  2024-08-14 17:12:03  24.05.20240615.752c634   6.6.33                          *

--- home/features/cli/fish/interactive-shell-init.nix
+++ home/features/cli/fish/interactive-shell-init.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}: {
   programs.fish.interactiveShellInit = ''
+    if not set -q TMUX
+        tmux attach -t default || tmux new -s default
+    end
     source (starship init fish --print-full-init | psub)
     nerdfetch
     enable_transience
