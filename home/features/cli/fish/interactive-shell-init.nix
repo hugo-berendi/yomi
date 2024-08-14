827e8050f473e548e3dc864a774660e04ce6e3a4
Hugo Berendi <hugo.berendi@outlook.de>
2024-08-14 02:35:02 +0200

    137 current  2024-08-14 02:34:39  24.05.20240615.752c634   6.6.33                          *

--- home/features/cli/fish/interactive-shell-init.nix
+++ home/features/cli/fish/interactive-shell-init.nix
@@ -1,6 +1,5 @@
 {pkgs, ...}: {
   programs.fish.interactiveShellInit = ''
-    source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
     source (starship init fish --print-full-init | psub)
     nerdfetch
     enable_transience
