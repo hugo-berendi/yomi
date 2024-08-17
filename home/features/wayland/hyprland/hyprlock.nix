71ceb8d7800ece7abd96caf1515ef20e653821c4
Hugo Berendi <hugo.berendi@outlook.de>
2024-08-18 00:37:18 +0200

    205 current  2024-08-18 00:37:16  24.05.20240815.c3d4ac7  6.6.45                          *

--- home/features/wayland/hyprland/hyprlock.nix
+++ home/features/wayland/hyprland/hyprlock.nix
@@ -48,7 +48,7 @@ in {
         dots_rounding = -1; # -1 default circle, -2 follow input-field rounding
         outer_color = "rgb(${config.satellite.theming.colors.rgb "base0D"})";
         inner_color = "rgb(${config.satellite.theming.colors.rgb "base01"})";
-        font_color = "rgb(${config.satellite.theming.colors.rgb "base05"})";
+        font_color = "rgb(${config.satellite.theming.colors.rgb "base06"})";
         fade_on_empty = false;
         fade_timeout = 1000; # Milliseconds before fade_on_empty is triggered.
         placeholder_text = "<i>Input Password...</i>"; # Text rendered in the input box when it's empty.
