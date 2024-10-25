af9b91add63a9ab80861a0a081fb89d08134bc65
hugo-berendi <git@hugo-berendi.de>
2024-10-25 15:27:19 +0200

    crazy shit here man 2.

---
+++ hosts/nixos/inari/services/sonarr.nix
@@ -0,0 +1,16 @@
+{config,...}: {
+  # This is the default port, and can only be changed via the GUI
+  yomi.nginx.at.sonarr.port = 8989;
+
+  services.sonarr = {
+    enable = true;
+  };
+
+  environment.persistence."/persist/state".directories = [
+    {
+      inherit (config.services.sonarr) user group;
+      directory = config.services.sonarr.dataDir;
+      mode = "u=rwx,g=r,o=r";
+    }
+  ];
+}
