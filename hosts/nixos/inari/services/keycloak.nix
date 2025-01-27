54fc76b157893892aa6e5c9831cd2ea17ae19ba0
hugo-berendi <git@hugo-berendi.de>
2025-01-27 19:12:42 +0100

    fix: keycloak

--- hosts/nixos/inari/services/keycloak.nix
+++ hosts/nixos/inari/services/keycloak.nix
@@ -12,10 +12,14 @@
     initialAdminPassword = "admin";
     settings = {
       hostname = "keycloak.hugo-berendi.de";
-      hostname-backchannel-dynamic = true;
+      # hostname-backchannel-dynamic = true;
+      # https-port = config.yomi.cloudflared.at.keycloak.port;
       http-port = config.yomi.cloudflared.at.keycloak.port;
+      http-enabled = true;
+      proxy-headers = "xforwarded";
     };
     database = {
+      type = "mariadb";
       passwordFile = config.sops.secrets.kc_database.path;
     };
   };
