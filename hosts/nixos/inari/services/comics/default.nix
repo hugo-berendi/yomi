971411c2e16892c3dd6885c1f92f795bea185c86
hugo-berendi <git@hugo-berendi.de>
2025-02-18 12:36:03 +0100

    fix suwayomi-server

--- hosts/nixos/inari/services/comics/default.nix
+++ hosts/nixos/inari/services/comics/default.nix
@@ -3,36 +3,28 @@
 in {
   yomi.cloudflared.at.suwayomi.port = 4567;
 
-  sops.secrets.suwayomi = {
-    sopsFile = ../secrets.yaml;
-    owner = config.services.suwayomi-server.user;
-    group = config.services.suwayomi-server.group;
-  };
-
   services.suwayomi-server = {
     enable = true;
-    user = "root";
     dataDir = comicDir;
     settings = {
       server = {
+        downloadAsCbz = true;
+        downloadsPath = comicDir;
+        autoDownloadNewChapters = true;
+
         port = config.yomi.cloudflared.at.suwayomi.port;
+
         extensionRepos = [
           "https://raw.githubusercontent.com/keiyoushi/extensions/repo/index.min.json"
           "https://raw.githubusercontent.com/suwayomi/tachiyomi-extension/repo/index.min.json"
         ];
+
+        flareSolverrEnabled = false;
       };
     };
   };
 
   # {{{ Storage
-  # environment.persistence."/persist/state".directories = [
-  #   {
-  #     inherit (config.services.suwayomi-server) user group;
-  #     directory = config.services.suwayomi-server.dataDir;
-  #     mode = "u=rwx,g=r,o=r";
-  #   }
-  # ];
-
   systemd.tmpfiles.rules = let
     dataDir = config.services.suwayomi-server.dataDir;
     user = {
@@ -40,8 +32,8 @@ in {
       group = config.services.suwayomi-server.group;
     };
   in [
-    "d ${dataDir}                 0755 ${user.name} ${user.group}"
-    "d ${dataDir}/.local/share/Tachidesk                 0755 ${user.name} ${user.group}"
+    "d ${dataDir}                         0755 ${user.name} ${user.group}"
+    "d ${dataDir}/.local/share/Tachidesk  0755 ${user.name} ${user.group}"
   ];
   # }}}
 }
