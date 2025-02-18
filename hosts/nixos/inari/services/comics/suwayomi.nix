f4a366d54c33ca7d5c08064ab9ad74242fa3c54e
hugo-berendi <git@hugo-berendi.de>
2025-02-18 12:38:49 +0100

    cleanup comic setup for future

---
+++ hosts/nixos/inari/services/comics/suwayomi.nix
@@ -0,0 +1,39 @@
+{config, ...}: let
+  comicDir = "/raid5pool/media/comics";
+in {
+  yomi.cloudflared.at.suwayomi.port = 4567;
+
+  services.suwayomi-server = {
+    enable = true;
+    dataDir = comicDir;
+    settings = {
+      server = {
+        downloadAsCbz = true;
+        downloadsPath = comicDir;
+        autoDownloadNewChapters = true;
+
+        port = config.yomi.cloudflared.at.suwayomi.port;
+
+        extensionRepos = [
+          "https://raw.githubusercontent.com/keiyoushi/extensions/repo/index.min.json"
+          "https://raw.githubusercontent.com/suwayomi/tachiyomi-extension/repo/index.min.json"
+        ];
+
+        flareSolverrEnabled = false;
+      };
+    };
+  };
+
+  # {{{ Storage
+  systemd.tmpfiles.rules = let
+    dataDir = config.services.suwayomi-server.dataDir;
+    user = {
+      name = config.services.suwayomi-server.user;
+      group = config.services.suwayomi-server.group;
+    };
+  in [
+    "d ${dataDir}                         0755 ${user.name} ${user.group}"
+    "d ${dataDir}/.local/share/Tachidesk  0755 ${user.name} ${user.group}"
+  ];
+  # }}}
+}
