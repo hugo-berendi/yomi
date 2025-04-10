1d9dc709be1fd4fd2a943472ac650f1a21b5d4b9
hugo-berendi <git@hugo-berendi.de>
2025-04-10 06:58:55 +0200

    cloudflared service now uses dynamic user

--- hosts/nixos/inari/services/cloudflared.nix
+++ hosts/nixos/inari/services/cloudflared.nix
@@ -6,8 +6,8 @@
   environment.systemPackages = [pkgs.cloudflared];
   sops.secrets.cloudflare_tunnel_credentials = {
     sopsFile = ../secrets.yaml;
-    owner = config.services.cloudflared.user;
-    group = config.services.cloudflared.group;
+    # owner = config.services.cloudflared.user;
+    # group = config.services.cloudflared.group;
   };
 
   yomi.cloudflared.tunnel = "39befb96-2f47-44c1-a748-a5516e0891ca";
