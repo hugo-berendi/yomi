60d4397cbaef4a50f48aab41a5af65bcd2c1212c
hugo-berendi <git@hugo-berendi.de>
2025-04-10 06:59:38 +0200

    radicale has change the default of `auth.type` to `denyall` (not good)

--- hosts/nixos/inari/services/radicale.nix
+++ hosts/nixos/inari/services/radicale.nix
@@ -6,6 +6,7 @@ in {
     enable = true;
 
     settings = {
+      auth.type = "none"; # default is denyall
       server.hosts = ["localhost:${toString port}"];
       storage.filesystem_folder = dataDir;
     };
