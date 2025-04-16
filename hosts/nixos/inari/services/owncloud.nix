5e7f38a2aa104a791389ad58dfe252774ac63ec1
hugo-berendi <git@hugo-berendi.de>
2025-04-16 19:01:35 +0200

    start workig on ocis

---
+++ hosts/nixos/inari/services/owncloud.nix
@@ -0,0 +1,32 @@
+{config, ...}: {
+  # {{{ secrets
+  sops.secrets.ocis_env = {
+    sopsFile = ../secrets.yaml;
+    owner = config.services.ocis.user;
+    group = config.services.ocis.group;
+  };
+  # }}}
+  # {{{ reverse proxy
+  yomi.nginx.at.cloud.port = config.yomi.ports.owncloud;
+  # }}}
+  # {{{ general config
+  services.ocis = {
+    enable = true;
+    environmentFile = config.sops.secrets.ocis_env.path;
+    address = "0.0.0.0";
+    port = config.yomi.ports.owncloud;
+    url = "https://cloud.hugo-berendi.de";
+    stateDir = "/raid5pool/cloud";
+    configDir = "/var/lib/ocis/config";
+  };
+  # }}}
+  # {{{ storage
+  environment.persistence."/persist/state".directories = [
+    {
+      directory = config.services.ocis.configDir;
+      user = config.users.users.ocis.name;
+      group = config.users.users.ocis.group;
+    }
+  ];
+  # }}}
+}
