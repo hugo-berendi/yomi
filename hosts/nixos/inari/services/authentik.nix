ccd3e1c4eccccf68fdb5f1a309abb8653fece90b
hugo-berendi <git@hugo-berendi.de>
2025-04-16 19:02:05 +0200

    add authentik

---
+++ hosts/nixos/inari/services/authentik.nix
@@ -0,0 +1,30 @@
+{config, ...}: {
+  # {{{ reverse proxy / domain setup
+  yomi.cloudflared.at.authentik.port = config.yomi.ports.authentik;
+  # }}}
+  # {{{ Secrets
+  sops.secrets.authentik_env = {
+    sopsFile = ../secrets.yaml;
+    # owner = config.users.users.authentik.name;
+    # group = config.users.users.authentik.group;
+  };
+  # }}}
+  # {{{ General config
+  services.authentik = {
+    enable = true;
+    environmentFile = config.sops.secrets.authentik_env.path;
+    settings = {
+      email = {
+        host = "smtp.migadu.com";
+        port = 587;
+        username = "authentik@tengu.hugo-berendi.de";
+        use_tls = true;
+        use_ssl = false;
+        from = "authentik@tengu.hugo-berendi.de";
+      };
+      disable_startup_analytics = true;
+      avatars = "initials";
+    };
+  };
+  # }}}
+}
