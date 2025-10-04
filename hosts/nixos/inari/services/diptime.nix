872018e91eb61b8361b11542d9dc5d4fdc7ee557
hugo-berendi <git@hugo-berendi.de>
2025-10-05 00:22:06 +0200

    refactor(inari): remove unused service configurations
    Migrate media services to nixarr module and remove standalone configurations for:
    - *arr suite (bazarr, prowlarr, radarr, readarr, sonarr, qbittorrent)
    - jellyfin (now managed by nixarr)
    - Various discontinued services (calibre, commafeed, diptime, glance, intray, keycloak, komga, minecraft, ngrok, ntfy, pounce, smos, tml, valheim, whoogle)

--- hosts/nixos/inari/services/diptime.nix
+++
@@ -1,9 +0,0 @@
-# I couldn't find a hosted version of this
-{pkgs, ...}: {
-  yomi.nginx.at.diptime.files = pkgs.fetchFromGitHub {
-    owner = "bhickey";
-    repo = "diplomatic-timekeeper";
-    rev = "d6ea7b9d9e94ee6d2db8e4e7cff5f8f1c3f04464";
-    sha256 = "09s6awz5m6hzpc6jp96c118i372430c7b41acm5m62bllcvrj9vk";
-  };
-}
