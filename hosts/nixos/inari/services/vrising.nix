21a1d9170b6f492b295f6647b04bc2b145bc2dca
hugo-berendi <git@hugo-berendi.de>
2025-11-20 08:19:48 +0100

    feat(vrising): add V Rising dedicated server with custom game settings

---
+++ hosts/nixos/inari/services/vrising.nix
@@ -0,0 +1,30 @@
+{...}: {
+  sops.secrets.vrising_rcon_password = {
+    sopsFile = ../secrets.yaml;
+  };
+
+  services.vrising = {
+    enable = true;
+    serverName = "FischGHGesicht";
+    worldName = "Yomi";
+    autosaveRetention = 6 * 60 * 60;
+    gameSettings = {
+      teleportBoundItems = false;
+      castleHeartLevel1FloorLimit = 60;
+      castleHeartLevel1HeightLimit = 3;
+      castleHeartLevel1ServantLimit = 6;
+      castleHeartLevel2FloorLimit = 160;
+      castleHeartLevel2HeightLimit = 4;
+      castleHeartLevel2ServantLimit = 10;
+      castleHeartLevel3FloorLimit = 400;
+      castleHeartLevel3HeightLimit = 7;
+      castleHeartLevel3ServantLimit = 14;
+      castleHeartLevel4FloorLimit = 600;
+      castleHeartLevel4HeightLimit = 8;
+      castleHeartLevel4ServantLimit = 18;
+      castleHeartLevel5FloorLimit = 1000;
+      castleHeartLevel5HeightLimit = 9;
+      castleHeartLevel5ServantLimit = 24;
+    };
+  };
+}
