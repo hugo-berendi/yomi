132a014f33ef1990e8ad33a5d9344a5374facfe1
hugo-berendi <git@hugo-berendi.de>
2026-01-12 22:48:32 +0100

    harden: apply standard tier to cloudflared tunnel

--- hosts/nixos/inari/services/cloudflared.nix
+++ hosts/nixos/inari/services/cloudflared.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: {
@@ -20,5 +21,11 @@
   };
 
   yomi.anubis.enable = true;
+
+  systemd.services."cloudflared-tunnel-${config.yomi.cloudflared.tunnel}".serviceConfig =
+    lib.mkMerge [
+      (lib.mapAttrs (_: lib.mkForce) config.yomi.hardening.presets.standard)
+      {}
+    ];
   # }}}
 }
