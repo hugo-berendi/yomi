0f9d80f5b29f3eceadfbe59e81098ad2c2795398
hugo-berendi <git@hugo-berendi.de>
2026-05-19 07:10:45 +0200

    fix windrose game server

--- hosts/nixos/inari/services/windrose.nix
+++ hosts/nixos/inari/services/windrose.nix
@@ -8,7 +8,9 @@
     serverName = "SuckDuck 67 Looksgay";
     maxPlayerCount = 4;
     directConnectionServerPort = config.yomi.ports.windrose-direct;
-    useDirectConnection = false;
+    useDirectConnection = true; # Enable direct connection
+    p2pProxyAddress = "0.0.0.0"; # Set P2P proxy address
+    inviteCode = "YOUR_SECRET_CODE"; # Add invite code
     serviceConfig = lib.mkMerge [
       config.yomi.hardening.presets.base
       {
